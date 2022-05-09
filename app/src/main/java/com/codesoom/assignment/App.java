a8a28d28acaf7b383e75833727ee15d0944f3680
choyumin01 <choyumin01@gmail.com>
2022-05-10 07:07:35 +0900

    feature(App): SpringApplication.run

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -2,24 +2,19 @@ package com.codesoom.assignment;
 
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
+@SpringBootApplication
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) throws IOException {
-        int port = 8000;
-        int backLog = 0;
-        String path = "/";
-
-        InetSocketAddress address = new InetSocketAddress(port);
-        HttpServer httpServer = HttpServer.create(address, backLog);
-        HttpHandler handler = new AssignmentHttpHandler();
-        httpServer.createContext(path, handler);
-        httpServer.start();
+        SpringApplication.run(App.class, args);
     }
 }
